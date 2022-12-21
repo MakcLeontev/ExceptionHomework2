f426c859222f3d3ff05963153aec0b228ef40d01
MaxLeontev <lmaks21@mail.ru>
2022-12-21 21:02:22 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] intArray = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        try {
+            int d = 1;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e.getMessage());
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
